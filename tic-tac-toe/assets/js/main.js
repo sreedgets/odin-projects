6ae8c9a1393f1a368b6cc9d38e995fb37eb9d4e9
Skyler <sreedgets@outlook.com>
2021-07-14 16:31:11 -0500

    Added win conditions and logic to check for wins

--- tic-tac-toe/assets/js/main.js
+++ tic-tac-toe/assets/js/main.js
@@ -1,19 +1,19 @@
 //Gameboard object
 const gameboard = (() => {
-    let _gameGrid = ['X',,,,,,,,,];
+    let _gameGrid = [,,,,,,,,,];
 
-    let getGameGrid = () => {
-        return _gameGrid;
+    let getGameGrid = (e) => {
+        return _gameGrid[e];
     };
 
     const resetGameGrid = () => {
         _gameGrid = [,,,,,,,,];
-        setBoard();
     };
 
     const setPiece = ( box, sign ) => {
         _gameGrid[box] = sign;
         setBoard();
+        console.log(gameController.checkWin(gameboard));
     };
 
     const setBoard = () => {
@@ -28,49 +28,91 @@ const gameboard = (() => {
     return {getGameGrid, resetGameGrid, setPiece, setBoard};
 })();
 
-//Display controller object
-const displayController = (() => {
-
-})();
 
 
+//Player controller
+const Player = (sign) => {
+    const _playerSign = sign;
+
+    let getPlayerSign = () => {
+        return _playerSign;
+    };
+
+    return {getPlayerSign};
+};
 
 //Handles the game's logic. Checks for win conditions.
 const gameController = (() => {
-    let roundCounter = 1;
+    let _player1 = Player('X');
+    let _playerAi = Player('O');
+    let _roundCounter = 1;
     let _boxes = document.querySelectorAll('div.box');
     let _boxesArr = Array.from(_boxes);
     const _resetButton = document.querySelector('button.cwosant');
 
+    const _checkRows = (board) => {
+        for (let i = 0; i < 3; i++) {
+            let row = [];
+            for (let j = i * 3; j < i * 3 + 3; j++) {
+                row.push(board.getGameGrid(j));
+            }
+    
+            if (row.every(e => e === 'X') || row.every(e => e === 'O')) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const _checkColumns = (board) => {
+        for (let i = 0; i < 3; i++) {
+            let column = [];
+            for (let j = i; j < 3; j++) {
+                column.push(board.getGameGrid(i + 3 * j));
+            }
+            if (column.every(e => e === 'X') || column.every(e => e === 'O')) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const _checkDiagonal = (board) => {
+        const diag1 = [gameboard.getGameGrid(0), gameboard.getGameGrid(4), gameboard.getGameGrid(8)];
+        const diag2 = [gameboard.getGameGrid(2), gameboard.getGameGrid(4), gameboard.getGameGrid(6)];
+
+        if (diag1.every(e => e === 'X') || diag2.every(e => e === 'O')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const checkWin = board => {
+        if (_checkRows(board) || _checkDiagonal(board)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     //Adds event listeners for boxes and reset button and sets up the board.
     const init = (() => {
         for (let i = 0; i < _boxesArr.length; i++) {
             _boxesArr[i].addEventListener('click', () => {
         
-                gameboard.setPiece(i, 'X');
+                gameboard.setPiece(i, _player1.getPlayerSign());
             });
         }
 
         _resetButton.addEventListener('click', () => {
             gameboard.resetGameGrid();
+            gameboard.setBoard();
         });
 
         gameboard.setBoard();
     })();
+    return {checkWin};
 })();
-
-
-
-//Player controller
-const player = () => {
-    const _playerSign = 'X';
-
-    let getPlayerSign = () => {
-        return _playerSign;
-    }
-
-    return {getPlayerSign};
-};
 
 
 //Scratchpad
@@ -89,5 +131,6 @@ const winCons = [
 ];
 
 //If roundCounter hits 9 then match is a draw
-let roundCounter = 1;
 
+//When checking columns, one X in spot 8 counts as a win
+//not picking up 2,4,6 diagonal wins
