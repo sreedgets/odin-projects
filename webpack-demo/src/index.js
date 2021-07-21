27be3ff7762f0a8d4bd8250ed2a930f863bf5410
Skyler <sreedgets@outlook.com>
2021-07-21 15:20:45 -0500

    Finish webpack tutorial

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,11 +1,30 @@
 import _ from 'lodash';
+import myName from './myName';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
 
 document.body.appendChild(component());
+
+
+
+function component2() {
+    const element = document.createElement('div');
+
+    element.innerHTML = myName('Cwosant');
+    return element;
+}
+
+document.body.appendChild(component2());
