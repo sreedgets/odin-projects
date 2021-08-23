c64a10326937f2fceb97f9e233b56a6e3bec85e1
Skyler <sreedgets@outlook.com>
2021-08-22 22:01:53 -0500

    Work on dropdown menu toggle

--- dropdown2/main.js
+++ dropdown2/main.js
@@ -7,6 +7,9 @@ const dropdownMenu = (() => {
     });
 
     function togDrop() {
+        getDropdowns.forEach(menu => {
+            menu.nextElementSibling.classList.remove('visible');
+        });
         this.nextElementSibling.classList.toggle('visible');
     }
 })();
