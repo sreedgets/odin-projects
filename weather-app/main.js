b8f33250934828d191ce1c885065da905b3794c1
Skyler <sreedgets@outlook.com>
2021-08-30 21:19:01 -0500

    Finish weather app logic. Should clean up code and style it

--- weather-app/main.js
+++ weather-app/main.js
@@ -1,5 +1,30 @@
 const searchBtn = document.querySelector('.search');
+const tempToggle = document.getElementById('temp-tog');
+let tempInF = true;
+
 searchBtn.addEventListener('click', getWeather);
+tempToggle.addEventListener('click', () => {
+    let getTemp = document.querySelector('#user-temp').textContent;
+    toggleTemp(getTemp);
+});
+
+function toggleTemp(t) {
+    let getTemp = document.querySelector('#user-temp');
+
+    if (tempInF === true) {
+        let newTemp = Math.round((t - 32) / 1.8);
+
+        getTemp.textContent = newTemp;
+
+        tempInF = false;
+    } else {
+        let newTemp = Math.round((t * 1.8) + 32);
+
+        getTemp.textContent = newTemp;
+
+        tempInF = true;
+    }
+}
 
 
 function getWeather() {
@@ -10,8 +35,20 @@ function getWeather() {
             return data.json();
         })
         .then(data => {
-            
+            console.log(data);
+
+            const loc = data.name;
+            const temp = kvToFh(data.main.temp);
+            const weather = data.weather[0].main;
+
+            showWeather(loc, temp, weather);
     });
+}
+
+function kvToFh(k) {
+    const f = Math.round(((k-273.15) * 1.8) + 32);
+
+    return f;
 }
 
 function buildUrl() {
@@ -21,11 +58,19 @@ function buildUrl() {
     return searchUrl;
 }
 
+function showWeather(loc, temp, weather = '') {
+    const getLocation = document.querySelector('.user-loc');
+    const getTemp = document.querySelector('.user-temp');
+    const getUserWeather = document.querySelector('.user-weather');
 
+    getLocation.textContent = loc;
+    getTemp.textContent = temp;
+    getUserWeather.textContent = weather;
+
+}
 
 
 function cwosantLog() {
     console.log('cwosant');
-    console.log(locationSearch);
 }
 
